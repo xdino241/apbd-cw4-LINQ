0758b5779af1e35f55f2dd0175fdf265ba170435
Jakub Mikolajczyk <jakubmikolajczyk214@gmail.com>
2026-03-31 21:18:59 +0200

    asd

--- obj/Debug/net9.0/LinqConsoleLab.PL.AssemblyInfo.cs
+++ obj/Debug/net9.0/LinqConsoleLab.PL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinqConsoleLab.PL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46745a6342d6036b37e84e4b7058764fb6e816f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinqConsoleLab.PL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinqConsoleLab.PL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
